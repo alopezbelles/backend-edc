cec7d7fe01215ab9b4148e1a3391c9d30fe179f2
Alex <alopezbelles@gmail.com>
2023-08-02 13:18:04 +0200

    create route to get all tasks from database

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 
-Route::get('/tasks', [TaskController::class, 'index']);
+// Route::get('/tasks', [TaskController::class, 'index']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
