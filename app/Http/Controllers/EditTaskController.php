6cb1f297b8604071460520dbaafd82fbed6ebe60
Alex <alopezbelles@gmail.com>
2023-08-03 09:43:47 +0200

    I extend the task edit controller to edit title and description

--- app/Http/Controllers/EditTaskController.php
+++ app/Http/Controllers/EditTaskController.php
@@ -11,11 +11,25 @@ class EditTaskController extends Controller
     public function update(Request $request, $id)
     {
         $data = $request->validate([
-            'status' => 'required|in:completed,not completed',
+            'status' => 'nullable|in:completed,not completed',
+            'title' => 'string|max:255',
+            'description' => 'string',
         ]);
 
         $task = Task::findOrFail($id);
-        $task->status = $data['status'];
+
+        if (isset($data['status'])) {
+            $task->status = $data['status'];
+        }
+
+        if (isset($data['title'])) {
+            $task->title = $data['title'];
+        }
+
+        if (isset($data['description'])) {
+            $task->description = $data['description'];
+        }
+
         $task->save();
 
         return response()->json($task, 200);
