9ad9332de898f96ae73396168aedf5ce0d6ae7ea
Alex <alopezbelles@gmail.com>
2023-08-04 11:13:09 +0200

    I recover the status prior to the env change

--- app/Http/Controllers/EditTaskController.php
+++ app/Http/Controllers/EditTaskController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\Task;
-
 
 class EditTaskController extends Controller
 {
     public function update(Request $request, $id)
     {
         $data = $request->validate([
-            'status' => 'required|in:completed,not completed',
+            'status' => 'nullable|in:completed,not completed',
+            'title' => 'string|max:255',
+            'description' => 'string',
         ]);
 
         $task = Task::findOrFail($id);
-        $task->status = $data['status'];
+
+        if (isset($data['status'])) {
+            $task->status = $data['status'];
+        }
+
+        if (isset($data['title'])) {
+            $task->title = $data['title'];
+        }
+
+        if (isset($data['description'])) {
+            $task->description = $data['description'];
+        }
+
         $task->save();
 
         return response()->json($task, 200);
